95606ef5508032f18ff54d604652c58b05b3762d
acary <info@andycary.com>
2020-02-27 14:51:18 -0600

    initialized web app with serenity bdd

---
+++ README.md
@@ -0,0 +1,16 @@
+# SpringRestAPI
+
+A REST API-driven Web Application using Spring Boot (Java) and testing with Serenity BDD.
+
+## Getting Started
+
+### Clone repository:
+1. From your intended location, run: ```git clone git@github.com:acary/SpringRestAPI.git```
+2. CD to the folder
+
+
+## Run app:
+1. Run:  ```./gradlew bootRun```
+
+## Run tests:
+1. Run: ```./gradlew test```
---
+++ src/main/java/com/webapp/restapi/RestApiController.java
@@ -0,0 +1,30 @@
+package com.webapp.restapi;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webapp.restapi.models.*;
+
+@RestController
+public class RestApiController {
+
+    // Hello
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World! This is the first endpoint!";
+    }
+
+    // Greeting
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Earthling") String name) {
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
