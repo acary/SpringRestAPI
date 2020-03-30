a7290c8be49df73eb9dcacd29ae6a53540d68c99
acary <info@andycary.com>
2020-03-30 16:38:54 -0500

    Add MongoDB and Hotels

--- src/main/java/com/webapp/restapi/RestApiApplication.java
+++ src/main/java/com/webapp/restapi/RestApiApplication.java
@@ -1,13 +1,22 @@
 package com.webapp.restapi;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class RestApiApplication extends SpringBootServletInitializer implements CommandLineRunner {
+	protected final Log logger = LogFactory.getLog(getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Application Started!");
+	}
 }
--- src/main/java/com/webapp/restapi/RestApiController.java
+++ src/main/java/com/webapp/restapi/RestApiController.java
@@ -1,16 +1,11 @@
 package com.webapp.restapi;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.webapp.restapi.models.*;
 import com.webapp.restapi.services.ProductService;
